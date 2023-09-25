856a3523b3a5a254f161c0ba81e050e18cb2d855
Johan Johnsson <johan@johnsson-net.se>
2023-09-25 09:08:59 +0200

    added menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("\nLINKEDLIST CHALLENGE");
+        Scanner input = new Scanner(System.in);
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("\nAvailable actions (select word or letter)");
+            System.out.println("(F)orward" +
+                    "\n(B)ackwards" +
+                    "\n(L)ist Places" +
+                    "\n(M)enu" +
+                    "\n(Q)uit");
+            String choice = input.nextLine();
+            switch (choice) {
+                case "F", "Forward":
+                    break;
+                case "B", "Backwards":
+                    break;
+                case "L", "List":
+                    break;
+                case "M", "Menu":
+                    break;
+                case "Q", "Quit":
+                    isRunning = false;
+                    break;
+            }
+        }
+
     }
 }
